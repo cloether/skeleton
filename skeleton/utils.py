1f706dcb08a67d385cf9ec4e85cf1aab0b963123
cloether <cloether@outlook.com>
2020-10-20 11:56:49 -0400

    added ISO_DATETIME constants to utils.py

--- skeleton/utils.py
+++ skeleton/utils.py
@@ -10,8 +10,9 @@ import pickle
 import re
 import sys
 from contextlib import contextmanager
-from datetime import datetime
+from datetime import datetime, timedelta
 from errno import EEXIST
+from math import ceil
 from string import Formatter
 
 from six import integer_types, iteritems, string_types, text_type
@@ -20,6 +21,7 @@ __all__ = (
     "as_bool",
     "as_number",
     "cwd",
+    "DateRange",
     "EPOCH",
     "is_file_newer_than_file",
     "ISO_DATETIME_FORMAT",
@@ -38,6 +40,148 @@ EPOCH = datetime(1970, 1, 1)
 
 ISO_DATETIME_STRING = "1970-01-01 00:00:00.000"
 ISO_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+
+class DateRange(object):
+  """Creates a lazy range of date or datetime objects.
+
+  Modeled after the Python 3 range type and has fast path
+  membership checking, lazy iteration, indexing and slicing.
+
+  Unlike range, DateRange allows an open ended range. Also
+  unlike range, it does not have an implicit step so it must be
+  provided.
+
+  Args:
+    start (datetime or date): Range Start Time.
+    stop (datetime or date): Range End Time.
+    step (datetime or date): Range Step Time.
+
+  Examples:
+    now = datetime.now().date()
+    for d in DateRange(
+        start=now - timedelta(weeks=1),
+        stop=now,
+        step=timedelta(days=1)
+    ):
+      print(d)
+  """
+
+  def __init__(self, start=None, stop=None, step=None):
+    if start is None:
+      raise TypeError("must provide starting point for DateRange.")
+    if step is None:
+      raise TypeError("must provide step for DateRange.")
+    if step == timedelta(0):
+      raise TypeError("must provide non-zero step for DateRange")
+    self.start = start
+    self.stop = stop
+    self.step = step
+    self._has_neg_step = self.step < timedelta(0)
+
+  def __repr__(self):
+    return "{!s}(start={!r}, stop={!r}, step={!r}".format(
+        self.__class__.__name__, self.start, self.stop, self.step
+    )
+
+  def __reversed__(self):
+    if self.stop:
+      return DateRange(self.stop, self.start, -self.step)
+    raise ValueError("cannot reverse infinite range")
+
+  def __len__(self):
+    if self.stop is None:
+      # it'd be nice if float('inf') could be returned
+      raise TypeError("infinite range")
+    calc = (
+        self.start - self.stop
+        if self._has_neg_step
+        else self.stop - self.start
+    )
+    return int(ceil(abs(calc.total_seconds() / self.step.total_seconds())))
+
+  def __contains__(self, x):
+    if self.stop is not None:
+      check = (
+          self.start >= x > self.stop
+          if self._has_neg_step
+          else self.start <= x < self.stop
+      )
+    else:
+      check = (
+          self.start >= x
+          if self._has_neg_step
+          else self.start <= x
+      )
+    if not check:
+      return False
+    difference = x - self.start
+    return difference.total_seconds() % self.step.total_seconds() == 0
+
+  def _check_stop(self, current):
+    if self._has_neg_step:
+      return current <= self.stop
+    return current >= self.stop
+
+  def __iter__(self):
+    current, stopping = self.start, self.stop is not None
+    while True:
+      if stopping and self._check_stop(current):
+        break
+      yield current
+      current += self.step
+
+  def __eq__(self, other):
+    if isinstance(other, DateRange):
+      return (
+          self.start == other.start
+          and self.stop == other.stop
+          and self.step == other.step
+      )
+    return NotImplemented
+
+  def __ne__(self, other):
+    if isinstance(other, DateRange):
+      return not self == other
+    return NotImplemented
+
+  def __getitem__(self, idx_or_slice):
+    if isinstance(idx_or_slice, int):
+      return self._getidx(idx_or_slice)
+    elif isinstance(idx_or_slice, slice):
+      return self._getslice(idx_or_slice)
+    raise TypeError(
+        "DateRange indices must be integers or slices not {0}".format(
+            idx_or_slice.__class__
+        )
+    )
+
+  def _getidx(self, idx):
+    if not self.stop and 0 > idx:
+      raise IndexError("Cannot negative index infinite range")
+    if self.stop and abs(idx) > len(self) - 1:
+      raise IndexError("DateRange index out of range")
+    if idx == 0:
+      return self.start
+    elif 0 > idx:
+      idx += len(self)
+    return self.start + (self.step * idx)
+
+  def _getslice(self, slice_):
+    s = slice_.start, slice_.stop, slice_.step
+    if s == (None, None, None) or s == (None, None, 1):
+      return DateRange(start=self.start, stop=self.stop, step=self.step)
+    start, stop, step = s
+    # seems redundant but we're converting None -> 0
+    start = start or 0
+    stop = stop or 0
+    step = step or 1  # use 1 here because of multiplication
+    if not self.stop and (0 > start or 0 > stop):
+      raise IndexError("cannot negative index infinite range")
+    new_step = self.step * step
+    new_start = self.start if not start else self[start]
+    new_stop = self.stop if not stop else self[stop]
+    return DateRange(start=new_start, stop=new_stop, step=new_step)
 
 
 def _parse_format_str(s):
