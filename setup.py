9aabda203d50a8322d13be6f8271c77e47599ed2
cloether <chad.loether@outlook.com>
2021-03-21 15:54:12 -0400

    updated classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ Examples:
 References:
   https://packaging.python.org/guides/distributing-packages-using-setuptools/
   https://setuptools.readthedocs.io/en/latest/setuptools.html
+  https://pypi.org/classifiers/
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
@@ -64,20 +65,28 @@ def __find_readme(basename="README", extensions=("rst", "md", "txt", "")):
   """
 
   def _readme_content_type(_filename, _default=None):
-    if _filename.endswith("rst"):
+    """Determine README file content type.
+
+    Notes:
+      Currently based off file extension.
+
+    Args:
+      _filename (str): README file name.
+      _default (str): Default content type.
+
+    Returns:
+      str: Content type of the README file.
+    """
+    if _filename.endswith(".rst"):
       return "text/x-rst"
-
-    if _filename.endswith("md"):
+    if _filename.endswith(".md"):
       return "text/x-markdown"
-
-    if _filename.endswith("txt"):
+    if _filename.endswith(".txt"):
       return "text"
-
     return _default
 
   long_description = None
   long_description_content_type = None
-
   for ext in extensions:
     filename = (
         basename
@@ -85,12 +94,10 @@ def __find_readme(basename="README", extensions=("rst", "md", "txt", "")):
         else "{0}.{1}".format(basename, ext)
     )
     filepath = path.join(ROOT, filename)
-
     if path.exists(filepath):
       long_description = __readfile(filepath)
       long_description_content_type = _readme_content_type(filename, "text")
       break
-
   return long_description, long_description_content_type
 
 
@@ -111,17 +118,90 @@ def __readlines(filepath, **kwargs):
   return list(__iterlines(filepath, **kwargs))
 
 
-PACKAGES = find_packages(exclude=("test*", "script*", "example*"))
+# class MakeCleanCommand(Command):
+#   """A custom command to delete all generated doc files.
+#   """
+#   description = "run 'make --clean' to delete generated doc files"
+#   user_options = [
+#       # The format is (long option, short option, description).
+#       ('build-dir=', None, 'path to docs build dir'),
+#   ]
+#
+#   def initialize_options(self):
+#     """Set default values for options.
+#     """
+#     # Each user option must be listed here with their default value.
+#     # noinspection PyAttributeOutsideInit
+#     self.build_dir = 'docs'
+#
+#   def finalize_options(self):
+#     """Post-process options.
+#     """
+#     if self.build_dir:
+#       assert path.exists(self.build_dir), (
+#           'Docs build dir %s does not exist.' % self.build_dir
+#       )
+#
+#   def run(self):
+#     """Run command.
+#     """
+#     command = (
+#         "cd %(build_dir)s || echo directory %(build_dir)s does not exist. | "
+#         "exit 1; make clean; cd .." % {"build_dir": self.build_dir}
+#     )
+#     self.announce('Running command: make --clean', level=INFO)
+#     check_call(command, shell=True)
+#     self.announce('Generated doc files cleaned from dir: %s' % self.build_dir,
+#                   level=INFO)
+#
+#
+# class RunTestsCommand(Command):
+#   """A custom command to run tests.
+#   """
+#   description = "run 'scripts/run-tests' to to run tests"
+#   user_options = [
+#       # The format is (long option, short option, description).
+#       ('test-runner', None, 'path to test runner script'),
+#   ]
+#
+#   def initialize_options(self):
+#     """Set default values for options.
+#     """
+#     # Each user option must be listed here with their default value.
+#     # noinspection PyAttributeOutsideInit
+#     self.test_runner = 'pytest'
+#
+#   def finalize_options(self):
+#     """Post-process options.
+#     """
+#     if not self.test_runner:
+#       assert self.test_runner, 'Missing Test Runner.'
+#
+#   def run(self):
+#     """Run Command.
+#     """
+#     command = "%s" % self.test_runner
+#     self.announce('Running test script: %s' % command, level=INFO)
+#     try:
+#       check_call(command, shell=True)
+#     except CalledProcessError as e:
+#       self.announce("%s" % e, level=ERROR)
+
+
+PACKAGES = find_packages(exclude=("doc*", "example*", "script*", "test*"))
 NAME = PACKAGES[0]
+
 METADATA = __find_meta(path.join(ROOT, NAME, "__version__.py"))
 
 AUTHOR = METADATA.get("author")
 AUTHOR_EMAIL = METADATA.get("author_email")
+
 CLASSIFIERS = [
-    "Development Status :: 1 - Planning",
+    "Development Status :: 2 - Pre-Alpha",
     "Natural Language :: English",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.6",
@@ -133,14 +213,17 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
-    "Topic :: Internet",
+    "Topic :: Software Development :: Libraries :: Python Modules"
 ]
+
 DESCRIPTION = METADATA.get("description")
+
 ENTRY_POINTS = {
     "console_scripts": [
         "{0}={0}.__main__:main".format(NAME)
     ]
 }
+
 EXTRAS_REQUIRE = {
     "docs": [
         "sphinx",
@@ -166,24 +249,41 @@ EXTRAS_REQUIRE = {
         "ipaddress"
     ],
 }
+
 INCLUDE_PACKAGE_DATA = False
+
 KEYWORDS = "{0} template".format(NAME)
+
 LICENSE = METADATA.get("license")
+
 LONG_DESCRIPTION, LONG_DESCRIPTION_CONTENT_TYPE = __find_readme()
+
 PACKAGE_DATA = {}
+
 PLATFORMS = "Posix; MacOS X; Windows"
+
 REQUIREMENTS = __readlines(path.join(ROOT, "requirements.txt"))
+
 SCRIPTS = None
+
 TITLE = METADATA.get("title")
+
 URL = METADATA.get("url")
+
 VERSION = METADATA["version"]
+
 ZIP_SAFE = False
+
 PROJECT_URLS = {
-    # currently only works for GitHub urls.
+    # TODO: support more than github-based repositories.
+    #   - bitbucket
+    #   - gitlab
+    #   - etc...
     "Source": URL,
     "Tracker": "{0}/issues".format(URL)
 }
 
+# setup options
 setup_options = dict(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
@@ -208,10 +308,10 @@ setup_options = dict(
     zip_safe=ZIP_SAFE
 )
 
+# compile module executable
 if "py2exe" in sys.argv:
-  # TODO: Test
-  # py2exe command
-  # https://github.com/aws/aws-cli/blob/develop/setup.py
+  # TODO: Test py2exe References:
+  #  https://github.com/aws/aws-cli/blob/develop/setup.py
   import py2exe  # noqa
 
   # py2exe specific options.
@@ -234,8 +334,11 @@ if "py2exe" in sys.argv:
           ],
       }
   }
-  setup_options["console"] = [path.join("bin", NAME)]
+  setup_options["console"] = [
+      path.join("bin", NAME)
+  ]
 
+# run setup
 setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
