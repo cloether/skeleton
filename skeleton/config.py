b4191c3a0beadc061c4b6d378d09280ecbd970d3
cloether <chad.loether@outlook.com>
2021-05-29 13:53:51 -0400

    added AppDirs class attribute to Configuration class in config.py

--- skeleton/config.py
+++ skeleton/config.py
@@ -12,6 +12,7 @@ import os
 import sys
 from collections import OrderedDict
 
+from appdirs import AppDirs
 from six import (
   PY2,
   ensure_binary,
@@ -21,7 +22,7 @@ from six import (
   text_type
 )
 
-from . import __app_id__
+from . import __app_id__, __author__, __title__, __version__
 from .const import (
   DEFAULT_MAX_POOL_CONNECTIONS,
   DEFAULT_POOLBLOCK,
@@ -43,24 +44,39 @@ LOGGER = logging.getLogger(__name__)
 
 def getenv(keys, default=None, drop_null=True):
   """Create Configuration from a environment variables.
+
+  Args:
+    keys (list or tuple or str): Environment variables to retrieve.
+    default: Default value to set for key when no value is found.
+    drop_null (bool): If True null values will be ignored.
+
+  Returns:
+    dict: Dict of environment variable key/vales.
   """
   config = {}
+  if not keys:
+    return config
+  if isinstance(keys, string_types):
+    keys = (keys,)
   for key in keys:
-    value = os.getenv(key, default)
+    value = os.getenv(key, None)
     if drop_null and (not value or value is None):
       continue
-    config[key] = value
+    config[key] = value if value is not None else default
   return config
 
 
 # pylint: disable=inconsistent-return-statements
 def import_string(import_name, silent=False):
-  """Imports an object based on a string.  This is useful if you want to
-  use import paths as endpoints or something similar.  An import path can
-  be specified either in dotted notation (``xml.sax.saxutils.escape``)
-  or with a colon as object delimiter (``xml.sax.saxutils:escape``).
+  """Imports an object based on a string.
 
-  If `silent` is True the return value will be `None` if the import fails.
+  This is useful if you want to use import paths as endpoints
+  or something similar.  An import path can be specified either
+  in dotted notation (``xml.sax.saxutils.escape``) or with a
+  colon as object delimiter (``xml.sax.saxutils:escape``).
+
+  If `silent` is True the return value will be `None` if the
+  import fails.
 
   Args:
     import_name: the dotted name for the object to import.
@@ -92,7 +108,6 @@ def import_string(import_name, silent=False):
     except AttributeError as e:
       traceback = sys.exc_info()[2]
       reraise(ImportError, e, traceback)
-
   except ImportError as e:
     if not silent:
       reraise(
@@ -154,26 +169,36 @@ class Configuration(dict):
       ('user_agent', __app_id__),
       ('verify', False),
   ])
+  APP_DIRS = AppDirs(__title__, __author__, __version__)
+  FILENAME = "{0}.json".format(__title__)
+  FILEPATH_SITE = os.path.join(APP_DIRS.site_config_dir, FILENAME)
+  FILEPATH_USER = os.path.join(APP_DIRS.user_config_dir, FILENAME)
+  SEARCH_PATHS = (FILEPATH_SITE, FILEPATH_USER, FILENAME)
 
   # noinspection PyMissingConstructor
   # pylint: disable=super-init-not-called
   def __init__(self, *args, **kwargs):
     # record user provided options
     self._user_options = self._make_options(*args, **kwargs)
-    # Merge the user_provided options onto the default options
+    # merge the user_provided options onto the default options
     config_vars = copy.copy(self.OPTIONS)
     config_vars.update(self._user_options)
-    # Set the attributes based on the config_vars
+    # set the attributes based on the config_vars
     dict.update(self, config_vars)
 
   def _make_options(self, *args, **kwargs):
     """Set configuration values.
+
+    Returns:
+      config: Configuration dictionary.
     """
     config = {}
+
     for key, value in iteritems(kwargs):
       if key not in self.OPTIONS:
         raise ValueError("invalid key: {0}".format(key))
       config[key] = value
+
     keys = self.keylist()
 
     # number of args should not be longer than allowed options
@@ -190,11 +215,12 @@ class Configuration(dict):
       # if a kwarg was specified for the arg, then error out.
       if keys[i] in config:
         raise TypeError(
-            "got multiple values for keyword argument: {0}".format(
+            "multiple values for keyword argument: {0}".format(
                 keys[i]
             )
         )
       config[keys[i]] = arg
+
     return config
 
   # compatibility
@@ -227,6 +253,12 @@ class Configuration(dict):
   @classmethod
   def from_object(cls, value):
     """Create configuration from a python object.
+
+    Args:
+      value (object): Configuration object.
+
+    Returns:
+      Configuration: Configuration instance.
     """
     if isinstance(value, string_types):
       value = import_string(value)
@@ -256,6 +288,9 @@ class Configuration(dict):
   @classmethod
   def from_file(cls, value):
     """Create Configuration from a file.
+
+    Returns:
+      Configuration: Configuration Instance.
     """
     with open(value) as fd:
       content = json.load(fd)
@@ -264,9 +299,12 @@ class Configuration(dict):
   @classmethod
   def from_env(cls, default=None, drop_null=True):
     """Create Configuration from a environment variables.
+
+    Returns:
+      Configuration: Configuration Instance.
     """
     return cls(**getenv(
-        cls.OPTIONS,
+        cls.keylist(),
         default=default,
         drop_null=drop_null
     ))
@@ -275,7 +313,7 @@ class Configuration(dict):
     """Update Configuration from environment variables.
     """
     dict.update(self, getenv(
-        self.OPTIONS,
+        self.keylist(),
         default=default,
         drop_null=drop_null
     ))
@@ -283,6 +321,9 @@ class Configuration(dict):
   @classmethod
   def get_env(cls, key, default=None):
     """Create Configuration from a environment variables.
+
+    Returns:
+      Configuration: Configuration Instance.
     """
     cls.validate_key(key)
     return cls(key=os.getenv(key, default))
@@ -290,6 +331,13 @@ class Configuration(dict):
   @classmethod
   def validate_key(cls, key, strict=True):
     """Validate configuration key.
+
+    Raises:
+      ValueError: Raised when key is not valid and strict
+        is set to True.
+
+    Returns:
+      bool: True if key is valid otherwise False.
     """
     if key not in cls.OPTIONS:
       if strict:
@@ -323,6 +371,9 @@ class Configuration(dict):
   # pylint: disable=unused-argument
   def as_list(self, **kwargs):
     """Return configuration as list of tuples.
+
+    Returns:
+      (list or tuple): List of Configuration keys and values.
     """
     return list(self.items())
 
@@ -366,6 +417,9 @@ class Configuration(dict):
 
   def getall(self, keys, default=None, strict=False):
     """Get configuration value.
+
+    Returns:
+      list: List of key values.
     """
     return [self.get(key, default=default, strict=strict) for key in keys]
 
@@ -408,11 +462,11 @@ class Configuration(dict):
     """
     # copy current attributes in config object.
     # pylint: disable=protected-access
-    config_options = copy.copy(self._user_options)
+    options = copy.copy(self._user_options)
     for other in reversed(others):
       # pylint: disable=protected-access
-      config_options.update(other._user_options)
-    return Configuration(**config_options)
+      options.update(other._user_options)
+    return Configuration(**options)
 
   # dump
 
